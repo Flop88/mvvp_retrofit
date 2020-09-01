89b099ab35ef97a8a4b416364623f240fac1b3d3
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-01 10:05:47 +0300

    Implement getCountries() method

--- app/src/main/java/ru/mvlikhachev/MainActivity.java
+++ app/src/main/java/ru/mvlikhachev/MainActivity.java
@@ -3,12 +3,53 @@ package ru.mvlikhachev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import ru.mvlikhachev.Model.CountryInfo;
+import ru.mvlikhachev.Model.Result;
+import ru.mvlikhachev.Service.CountryService;
+import ru.mvlikhachev.Service.RetrofitInstance;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Result> resultArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getCountries();
+    }
+
+    private Object getCountries() {
+        CountryService countryService = RetrofitInstance.getService();
+        Call<CountryInfo> call = countryService.getResults();
+        call.enqueue(new Callback<CountryInfo>() {
+            @Override
+            public void onResponse(Call<CountryInfo> call, Response<CountryInfo> response) {
+                CountryInfo countryInfo = response.body();
+
+                if(countryInfo != null && countryInfo.getRestResponse() != null) {
+                    resultArrayList = (ArrayList<Result>) countryInfo.getRestResponse().getResult();
+                }
+
+                for (Result result: resultArrayList) {
+                    Log.d("resultArray", result.getName());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<CountryInfo> call, Throwable t) {
+
+            }
+        });
+        return resultArrayList;
     }
 }
